b804bb5a5c2ae1ded32c780089ac3da460539447
nazma98 <nazmasarker1311@gmail.com>
2024-12-10 21:42:55 +0600

    Add name entry widget

--- greeting.py
+++ greeting.py
@@ -7,4 +7,7 @@ root.geometry("400x300")
 instructions_label = tk.Label(root, text="Enter your name below:", font=("Arial", 14))
 instructions_label.pack(pady=10)
 
+name_entry = tk.Entry(root, font=("Arial", 14))
+name_entry.pack(pady=5)
+
 root.mainloop()
