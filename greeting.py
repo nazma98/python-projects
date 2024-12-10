5c74881c051cadadf1cdd08db48fab8e957dca89
nazma98 <nazmasarker1311@gmail.com>
2024-12-10 21:56:29 +0600

    Add greeting label

--- greeting.py
+++ greeting.py
@@ -13,4 +13,7 @@ name_entry.pack(pady=5)
 greet_button = tk.Button(root, text="Greet Me", command=greet_user, font=("Arial", 12))
 greet_button.pack(pady=5)
 
+greeting_label = tk.Label(root, text="", font=("Arial", 14), fg="blue")
+greeting_label.pack(pady=10)
+
 root.mainloop()
