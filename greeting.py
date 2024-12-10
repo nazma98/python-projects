6ec0ce4f1750085d08ed78757095d0f9f0f4454b
nazma98 <nazmasarker1311@gmail.com>
2024-12-10 21:31:56 +0600

    Add label for entering name

--- greeting.py
+++ greeting.py
@@ -4,4 +4,7 @@ root = tk.Tk()
 root.title("Greeting App")
 root.geometry("400x300")
 
+instructions_label = tk.Label(root, text="Enter your name below:", font=("Arial", 14))
+instructions_label.pack(pady=10)
+
 root.mainloop()
